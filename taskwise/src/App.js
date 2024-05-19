7e5621335c8c9dc996864091c13191fe787af82f
pranitak1 <52026606+pranitak1@users.noreply.github.com>
2024-05-19 15:09:35 +0530

    Update App.js

--- taskwise/src/App.js
+++ taskwise/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App" data-testid="app-component">
-      <h1>Welcome To TaskWiseUI ....</h1>
+      <h1>Welcome To TaskWiseUI TEST....</h1>
     </div>
   );
 }
